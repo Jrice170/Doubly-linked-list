0810a688b5e871a820a8ae6f0f87fa3246847d8c
Joseph Michael Rice <josephrice170@gmail.com>
2020-01-08 10:26:18 -0600

    Add files via upload

--- pa05.cpp
+++ pa05.cpp
@@ -7,59 +7,81 @@
 // Use this to test, but not for your actual program.
 #include <list>
 
-// This is yours 
+// This is yours
 #include "MyList.h"
+
+template <typename T>
+void sort(MyList<T> & list_object); // will sort a dubly linked list
+
+
+
 
 int main()
 {
-    MyList<int> l;
-    //std::list<int> l;
-    
-    // Make your hpp file with empty definitions other than your constructor
-    // Build one function at a time, and only add one "test" line at a time to this file.
-    // Uncomment and add lines below here 1 at a time.
-    // This file is just an example, and may not behave as expected!
-    // Test all your MyList functions here or in multiple mains (I suggest one function test per file)
 
-    /*
-    l.push_back(4000);
-    l.push_back(200);
-    l.push_back(100);
-    cout << l.front() << endl;
-    l.front() = 33;
-    cout << l.front() << endl;
-    cout << l.back() << endl;
 
-        cout << l.size() << endl;
-    l.push_back(4000);
-    l.push_back(200);
-    l.push_back(100);
-    cout << l.front() << endl;
-    cout << l.back() << endl;
-    
-    l.push_front(423);
 
-    cout << l.front() << endl;
-    
-    MyList<int> sink;
-    sink = l;
-    cout << sink.front() << endl;
-    cout << sink.back() << endl;
-    
-    l.insert(l.begin(), 3);
-    l.insert(l.end(), 20);
-    l.pop_front();
-    l.reverse();
+    MyList<string> String_List;
+    String_List.insert(0,"hhhh");
+    MyList<int> un_ordered;
+    un_ordered.insert(0,2);
+    un_ordered.insert(1,5);
+    un_ordered.insert(2,4);
+    un_ordered.insert(3,9);
+    un_ordered.Display_all();
+    sort(un_ordered);
+    un_ordered.Display_all();
 
-    int j = 0;
-    for(auto i = 0; i < l.size(); i++)
-    {
-        cout << l.back() << endl;
-        l.pop_back();
-        j++;
-    }
-    */
-    
-    return 0;
+
+
+
 }
 
+template<typename T>
+void sort(MyList<T> & list_object)
+{
+
+    int give_num = list_object.size();
+    T array[give_num];
+    for(int i = 0; i < give_num;i++)
+    {
+
+
+        array[i] = list_object.front();
+        list_object.pop_front();
+
+
+    }
+    T Key;
+    int i =0 ;
+
+    for(int j = 1; j < give_num; j++)
+    {
+
+        Key = array[j];
+
+        i = (j-1);
+        while((i >= 0) && (array[i] > Key))
+        {
+
+              array[i+1] = array[i];
+              i = (i - 1);
+              array[i+1] = Key;
+
+        }
+
+
+    }
+
+  
+    list_object.clear();
+    for(int i=0;i < give_num; i++)
+    {
+
+        list_object.insert(i,array[i]);
+
+
+
+    }
+
+}
